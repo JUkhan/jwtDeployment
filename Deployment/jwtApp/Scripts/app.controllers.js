7eb27f987864d0f175f660165cca03ce1a4e7077
jasim <jasim@gmail.com>
2015-05-06 15:38:02 +0600

    init

--- Deployment/jwtApp/Scripts/app.controllers.js
+++ Deployment/jwtApp/Scripts/app.controllers.js
@@ -3,9 +3,8 @@ import home from 'Scripts/Components/home/homeCtrl.js';
 import login from 'Scripts/Components/login/loginCtrl.js';
 import signup from 'Scripts/Components/signup/signupCtrl.js';
 import associate from 'Scripts/Components/associate/associateCtrl.js';
-import widget007 from 'Scripts/Components/widget007/widget007Ctrl.js';
 import root from 'Scripts/Layouts/root/rootCtrl.js';
-import sublayout from 'Scripts/Layouts/sublayout/sublayoutCtrl.js';
+
 
 var moduleName='app.controllers';
 
@@ -14,8 +13,7 @@ angular.module(moduleName,[])
 .controller('loginCtrl', login)
 .controller('signupCtrl', signup)
 .controller('associateCtrl', associate)
-.controller('widget007Ctrl', widget007)
-.controller('rootCtrl', root)
-.controller('sublayoutCtrl', sublayout);
+.controller('rootCtrl', root);
+
 
 export default moduleName;
