097ce265fc65286ae2a8dabf70b5bbd773ef6e7b
JUkhan <jasim.uddin.khan@gmail.com>
2015-09-27 01:13:52 +0600

    pricing app editor

---
+++ pricing/Scripts/app.controllers.js
@@ -0,0 +1,27 @@
+
+import home from 'Scripts/Components/home/homeCtrl.js';
+import login from 'Scripts/Components/login/loginCtrl.js';
+import signup from 'Scripts/Components/signup/signupCtrl.js';
+import associate from 'Scripts/Components/associate/associateCtrl.js';
+import chatWidget from 'Scripts/Components/chatWidget/chatWidgetCtrl.js';
+import WidgetViewRights from 'Scripts/Components/WidgetViewRights/WidgetViewRightsCtrl.js';
+import userInRoles from 'Scripts/Components/userInRoles/userInRolesCtrl.js';
+import Person from 'Scripts/Components/Person/PersonCtrl.js';
+import Products from 'Scripts/Components/Products/ProductsCtrl.js';
+import root from 'Scripts/Layouts/root/rootCtrl.js';
+
+var moduleName='app.controllers';
+
+angular.module(moduleName,[])
+.controller('homeCtrl', home)
+.controller('loginCtrl', login)
+.controller('signupCtrl', signup)
+.controller('associateCtrl', associate)
+.controller('chatWidgetCtrl', chatWidget)
+.controller('WidgetViewRightsCtrl', WidgetViewRights)
+.controller('userInRolesCtrl', userInRoles)
+.controller('PersonCtrl', Person)
+.controller('ProductsCtrl', Products)
+.controller('rootCtrl', root);
+
+export default moduleName;
---
+++ pricing/Scripts/app.services.js
@@ -0,0 +1,21 @@
+
+import home from 'Scripts/Components/home/homeSvc.js';
+import chatWidget from 'Scripts/Components/chatWidget/chatWidgetSvc.js';
+import WidgetViewRights from 'Scripts/Components/WidgetViewRights/WidgetViewRightsSvc.js';
+import userInRoles from 'Scripts/Components/userInRoles/userInRolesSvc.js';
+import Person from 'Scripts/Components/Person/PersonSvc.js';
+import Products from 'Scripts/Components/Products/ProductsSvc.js';
+import root from 'Scripts/Layouts/root/rootSvc.js';
+
+var moduleName='app.services';
+
+angular.module(moduleName,[])
+.factory('homeSvc', home)
+.factory('chatWidgetSvc', chatWidget)
+.factory('WidgetViewRightsSvc', WidgetViewRights)
+.factory('userInRolesSvc', userInRoles)
+.factory('PersonSvc', Person)
+.factory('ProductsSvc', Products)
+.factory('rootSvc', root);
+
+export default moduleName;
